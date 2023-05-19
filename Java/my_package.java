f3c4521ee25d1a8175f783ed55bd7b1e0ebf56cb
BhaviniKorthi <bhavini.korthi@iitgn.ac.in>
2023-05-19 13:08:35 +0530

    packages

---
+++ Java/my_package.java
@@ -0,0 +1,9 @@
+package packages;
+public class my_package {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello, you can use this package to test your code");
+    }
+    
+}
