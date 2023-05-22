ea238278ebbab1c2df41e77c344e4f250ad8a2f6
BhaviniKorthi <bhavini.korthi@iitgn.ac.in>
2023-05-22 10:45:02 +0530

    Threads

--- Java/Thread_.java
+++ Java/Thread_.java
@@ -1,18 +1,38 @@
-public class Thread_ implements Runnable {
+class ThreadRun implements Runnable {
+    Thread t;
+    String name;
 
-    public void run(){
-        System.out.println("Thread is running");
+    ThreadRun(String name) {
+        this.name = name;
+        t = new Thread(this, name);
+        t.start();
     }
+
+    public void run() {
+        System.out.println("Thread " + this.name + " is running");
+        for (int i = 0; i < 10; i++) {
+            try{
+                if (i==5 && this.name.equals("T-1")){
+                    t.sleep(10000);
+                    this.name = "Updated t-1";
+                    t.setName(this.name);
+                }
+            }catch(InterruptedException e){
+                System.out.println("Exception occured..."+e);
+            }
+            System.out.println("Thread " + this.name + " is running " + i);
+        }
+    }
+
+}
+
+public class Thread_ {
     public static void main(String[] args) {
-        Runnable t1 = new Thread_();
-        // Runnable t2 = new Thread_();
-
-        Thread t2 = new Thread(t1, "Create new thread");
-
+        ThreadRun t1 = new ThreadRun("T-1");
         // t1.start();
+
+        ThreadRun t2 = new ThreadRun("T-2");
         // t2.start();
-        System.out.println(t2.getName());
-        t2.start();
+
     }
-    
 }
