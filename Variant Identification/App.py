42e9f0821d1acfcf5281d899729b1a6a39cc7652
BhaviniKorthi <bhavini.korthi@iitgn.ac.in>
2023-06-11 11:02:28 +0530

    Redis cached layer

--- Variant Identification/App.py
+++ Variant Identification/App.py
@@ -4,6 +4,7 @@ from View.view import VariantAPI
 from Controller.controller import RouteHandler
 from Data_populate import Populate
 import mysql.connector
+import redis
 
 app = Flask(__name__)
 config = {
@@ -15,11 +16,14 @@ config = {
 }
 
 db_connection = mysql.connector.connect(**config)
-variant_handler = VariantHandler(db_connection)
+redis_pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(connection_pool=redis_pool)
+
+variant_handler = VariantHandler(db_connection, redis_client)
 variant_handler.create_table()
 data = Populate(db_connection)
 data.load_data()
-variant_api = VariantAPI(variant_handler)
+variant_api = VariantAPI(variant_handler, redis_client)
 route_handler = RouteHandler(variant_api)
 
 @app.route('/')
--- Variant Identification/Model/model.py
+++ Variant Identification/Model/model.py
@@ -1,9 +1,10 @@
 import json
+import hashlib
 
 class VariantHandler:
-    def __init__(self, db_connection):
+    def __init__(self, db_connection, redis_client):
         self.db_connection = db_connection
-
+        self.redis_client = redis_client
 
     def create_table(self):
         create_table_query = """
@@ -19,7 +20,6 @@ class VariantHandler:
         self.db_connection.commit()
         db_cursor.close()
 
-
     def load_data(self):
         query = "SELECT COUNT(*) AS count FROM variants"
         db_cursor = self.db_connection.cursor(dictionary=True)
@@ -28,8 +28,8 @@ class VariantHandler:
         # db_cursor.close()
         if result == 0:
             count = 100
-            insert_query = "INSERT INTO variants (variant_info, variant_hash) VALUES (%s, MD5(%s))"
-            check_query = "SELECT variant_id FROM variants WHERE variant_hash = MD5(%s)"
+            insert_query = "INSERT INTO variants (variant_info, variant_hash) VALUES (%s, %s)"
+            check_query = "SELECT variant_id FROM variants WHERE variant_hash = %s"
             db_cursor = self.db_connection.cursor(dictionary=True)
             
             for i in range(1, count + 1):
@@ -38,40 +38,52 @@ class VariantHandler:
                     'description': f'Description for Variant {i}',
                 }
                 variant_info = json.dumps(variant_data)
+                variant_hash = hashlib.md5(variant_info.encode()).hexdigest()
 
-                db_cursor.execute(check_query, (variant_info,))
+                db_cursor.execute(check_query, (variant_hash,))
                 count = db_cursor.fetchall()
 
                 if len(count) > 0:
                     continue
                 else:
-                    db_cursor.execute(insert_query, (variant_info, variant_info))
+                    db_cursor.execute(insert_query, (variant_info, variant_hash))
                     self.db_connection.commit()
 
         db_cursor.close()
 
-
     def get_variant_by_id(self, variant_id):
+        variant_info = self.redis_client.get(f'variant:{variant_id}')
+        if variant_info:
+            return variant_info
         query = "SELECT variant_info AS info FROM variants WHERE variant_id = %s"
         db_cursor = self.db_connection.cursor(dictionary=True)
         db_cursor.execute(query, (variant_id,))
         result = db_cursor.fetchone()
+
         if result:
             variant_info = result['info']
+            self.redis_client.set(f'variant:{variant_id}', variant_info)
             return variant_info
         else:
             return None
 
-    def get_variant_by_info(self, variant_info):  #need to be fixed
-        variant_info_str = json.dumps(variant_info) 
-        query = "SELECT variant_id FROM variants WHERE variant_hash = MD5(%s)"
+    def get_variant_by_info(self, variant_info):
+        variant_info_str = json.dumps(variant_info)
+        variant_hash = hashlib.md5(variant_info_str.encode()).hexdigest()
+
+        variant_id = self.redis_client.get(f'variant_hash:{variant_hash}')
+        if variant_id:
+            return variant_id
+
+        query = "SELECT variant_id FROM variants WHERE variant_hash = %s"
         db_cursor = self.db_connection.cursor(dictionary=True)
-        db_cursor.execute(query, (variant_info_str,))
+        db_cursor.execute(query, (variant_hash,))
         results = db_cursor.fetchall()
 
         if results:
             if len(results) == 1:
                 variant_id = results[0]['variant_id']
+                self.redis_client.set(f'variant_hash:{variant_hash}', variant_id)
                 return variant_id
             elif len(results) > 1:
                 for result in results:
@@ -80,17 +92,23 @@ class VariantHandler:
                     db_cursor.execute(query, (variant_id,))
                     fetched_variant_info = db_cursor.fetchone()
                     if fetched_variant_info and json.loads(fetched_variant_info['variant_info']) == variant_info:
+                        self.redis_client.set(f'variant_hash:{variant_hash}', variant_id)
                         return variant_id
 
         return None
 
     def add_variant(self, variant_info):
-        variant_info_str = json.dumps(variant_info) 
+        variant_info_str = json.dumps(variant_info)
+        variant_hash = hashlib.md5(variant_info_str.encode()).hexdigest()
 
-        check_query = "SELECT variant_id FROM variants WHERE variant_hash = MD5(%s)"
-        insert_query = "INSERT INTO variants (variant_info, variant_hash) VALUES (%s, MD5(%s))"
+        variant_id = self.redis_client.get(f'variant_hash:{variant_hash}')
+        if variant_id:
+            return variant_id, "already exists"
+
+        check_query = "SELECT variant_id FROM variants WHERE variant_hash = %s"
+        insert_query = "INSERT INTO variants (variant_info, variant_hash) VALUES (%s, %s)"
         db_cursor = self.db_connection.cursor(dictionary=True)
-        db_cursor.execute(check_query, (variant_info_str,))
+        db_cursor.execute(check_query, (variant_hash,))
         result = db_cursor.fetchall()
 
         if result:
@@ -100,9 +118,12 @@ class VariantHandler:
                 db_cursor.execute(collision_query, (variant_id,))
                 fetched_variant_info = db_cursor.fetchone()
                 if fetched_variant_info and json.loads(fetched_variant_info['variant_info']) == variant_info:
+                    self.redis_client.set(f'variant_hash:{variant_hash}', variant_id)
                     return variant_id, "already exists"
 
-        db_cursor.execute(insert_query, (variant_info_str, variant_info_str))
+        db_cursor.execute(insert_query, (variant_info_str, variant_hash))
         self.db_connection.commit()
         variant_id = db_cursor.lastrowid
+        self.redis_client.set(f'variant_hash:{variant_hash}', variant_id)
         return variant_id, "added"
+g
