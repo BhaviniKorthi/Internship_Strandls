3b1dd72a563693cdac69be10fb1270127b51c433
BhaviniKorthi <bhavini.korthi@iitgn.ac.in>
2023-06-11 11:03:32 +0530

    redis update

--- Variant Identification/View/view.py
+++ Variant Identification/View/view.py
@@ -6,17 +6,8 @@ class VariantAPI:
         self.redis_client = redis_client
 
     def get_variant(self, variant_id):
-        # # Check if the variant exists in the cache
-        # variant_info = self.redis_client.get(f'variant:{variant_id}')
-        # if variant_info:
-        #     return render_template('variant.html', variant_id=variant_id, variant_info=variant_info.decode(),
-        #                            Message="Variant ID found")
-
-        # If not in cache, retrieve the variant from the database
         variant_info = self.variant_handler.get_variant_by_id(variant_id)
         if variant_info:
-            # Cache the variant for future requests
-            # self.redis_client.set(f'variant:{variant_id}', variant_info)
             return render_template('variant.html', variant_id=variant_id, variant_info=variant_info,
                                    Message="Variant ID found")
         else:
