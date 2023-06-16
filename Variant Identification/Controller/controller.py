988deee97e8030be2531c5edd13a01d00001735b
BhaviniKorthi <bhavini.korthi@iitgn.ac.in>
2023-06-16 14:25:45 +0530

    multiple inputs

--- Variant Identification/Controller/controller.py
+++ Variant Identification/Controller/controller.py
@@ -8,41 +8,35 @@ class RouteHandler:
     def home_page(self):
         return render_template('index.html')
 
-    def get_variant_info(self):
-        variant_id = request.args.get('variant_id')
-        if not variant_id:
+    def get_variant_info(self):  #get the variant info from the database
+        variant_ids = request.args.getlist('variant_id')
+        if len(variant_ids)==1 and variant_ids[0]=="":
             return jsonify({'error': 'Empty input is not allowed'})
-        
         try:
-            variant_id = int(variant_id)
-            return self.variant_api.get_variant(variant_id)
-        except ValueError:
+            variant_ids = [int(variant_id) for variant_id in variant_ids]  #convert the variant ids to int
+        except:
+            return jsonify({'error': 'Invalid input format' })  
+        return self.variant_api.get_info(variant_ids)
+
+
+    def get_variant_id(self): #get the variant id from the database
+        variant_infos = request.args.getlist('variant_info')
+        if len(variant_infos)==1 and variant_infos[0]=="":
+            return jsonify({'error': 'Empty input is not allowed'})
+        try:
+            variant_infos = [json.dumps(json.loads(variant_info))  for variant_info in variant_infos] #convert the variant infos to json    
+        except:
             return jsonify({'error': 'Invalid input format'})
         
+        return self.variant_api.get_id(variant_infos)
 
-
-    def get_variant_id(self):
-        variant_info = request.args.get('variant_info')
-        if not variant_info:
+    def add_variant(self): #add the variant to the database
+        variant_infos = request.form.getlist('add_entry')
+        if len(variant_infos)==1 and variant_infos[0]=="":
             return jsonify({'error': 'Empty input is not allowed'})
-        
         try:
-            variant_info = json.loads(variant_info)
-            return self.variant_api.find_variant(variant_info)
-        except json.JSONDecodeError:
+            variant_infos = [json.dumps(json.loads(variant_info)) for variant_info in variant_infos]
+        except:
             return jsonify({'error': 'Invalid input format'})
+        return self.variant_api.add_variant(variant_infos)
 
-
-
-
-    def add_variant(self):
-        variant_info = request.form.get('add_entry')
-        if not variant_info:
-            return jsonify({'error': 'Empty input is not allowed'})
-        
-        try:
-            variant_info = json.loads(variant_info)
-            return self.variant_api.add_variant(variant_info)
-        except json.JSONDecodeError:
-            return jsonify({'error': 'Invalid input format'})
-     
--- Variant Identification/View/view.py
+++ Variant Identification/View/view.py
@@ -5,26 +5,16 @@ class VariantAPI:
         self.variant_handler = variant_handler
         self.redis_client = redis_client
 
-    def get_variant(self, variant_id):
-        variant_dto = self.variant_handler.get_variant_by_id(variant_id)
-        if variant_dto is None:
-            return jsonify({'error': 'Variant ID not found'})
-        
-        return render_template('variant.html', variant_id=variant_dto.variant_id, variant_info=variant_dto.variant_info,
-                                   Message=variant_dto.Message)
+    def get_info(self, variant_id):
+        variant_dto = self.variant_handler.get_variant_by_id(variant_id)        
+        return render_template('variant.html', variant_results = variant_dto)
+    
 
-
-
-    def find_variant(self, variant_info):
-        variant_dto = self.variant_handler.get_variant_by_info(variant_info)
-        if variant_dto is None:
-            return jsonify({'error': 'Variant info not found'})
-        
-        return render_template('variant.html', variant_id=variant_dto.variant_id, variant_info=variant_dto.variant_info,
-                                   Message=variant_dto.Message)
+    def get_id(self, variant_info):
+        variant_dto = self.variant_handler.get_variant_by_info(variant_info)        
+        return render_template('variant.html', variant_results = variant_dto)
 
 
     def add_variant(self, variant_info):
         variant_dto= self.variant_handler.add_variant(variant_info)
-        return render_template('variant.html', variant_id=variant_dto.variant_id, variant_info=variant_dto.variant_info,
-                                   Message=variant_dto.Message)
+        return render_template('variant.html', variant_results = variant_dto)
